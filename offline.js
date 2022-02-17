c019c91e5eb298ab18340c43bdfa82b7174fac19
Christian Collins <99655989+Dabluew@users.noreply.github.com>
2022-02-16 22:12:50 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1645067446,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/titlebackground.png",
+		"images/title-sheet0.png",
+		"images/playbutton-sheet0.png",
+		"images/alarm-sheet0.png",
+		"images/textcover-sheet0.png",
+		"images/bedroomfloor.png",
+		"images/wall-sheet0.png",
+		"images/walsegment-sheet0.png",
+		"images/bed-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/rightbutton-sheet0.png",
+		"media/alarm.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
